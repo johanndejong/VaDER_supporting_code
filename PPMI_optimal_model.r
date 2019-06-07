53219a79f6bbab6bcf946f2b4fedf09419d3b605
Johann de Jong <johanndejong@hotmail.com>
2019-06-07 13:17:01 +0200

    Updated *_optimal_model.r scripts

--- PPMI_optimal_model.r
+++ PPMI_optimal_model.r
@@ -129,12 +129,14 @@ batch_size <- 16
 n_layer <- 2
 n_hidden1 <- 128
 n_hidden2 <- 32
-f_in <- file.path("..", "data", "PPMI", "PPMI.RData")
+# f_in <- file.path("..", "data", "PPMI", "PPMI.RData")
+f_in <- file.path("PPMI_artificial_data.RData")
 vader_path <- "../VaDER/"
 
 load(f_in)
 W <- 1 - MNAR
 dimnames(X)[[3]] <- dimnames(W)[[3]] <- toupper(dimnames(X)[[3]])
+X[W == 0] <- 0 # set missing values arbitrarily to 0 (can be any value)
 
 # The final model, on all data
 VADER <- import_from_path("vader", path = vader_path)$VADER
@@ -153,7 +155,7 @@ vader <- VADER(
   seed = 123L, # makes it reproducible
   recurrent = TRUE,
   # n_thread = 1L,
-  weights = W
+  weights = W # represents the missing values
 )
 vader$pre_fit(n_epoch = as.integer(100), verbose = TRUE)
 vader$fit(n_epoch = as.integer(100), verbose = TRUE)
@@ -208,3 +210,104 @@ for (i in 1:dim(X)[3]) {
 }
 par(mfrow = c(1, 1))
 dev.off()
+
+
+#############################################################
+# Use VaDER to simulate patient data from the optimal model #
+#############################################################
+
+X_train <- X
+W_train <- W
+Xy <- vader$generate(as.integer(dim(X_train)[1]))
+y_sim <- Xy$clusters
+X_sim <- Xy$samples
+dimnames(X_sim) <- dimnames(X_train)
+W_sim <- W_train # generate_missingness_pattern(X_sim, k = 0.8)
+mode(W_sim) <- "integer"
+dimnames(W_sim) <- dimnames(X_train)
+
+save_path <- file.path("vader_sim", "vader.ckpt")
+vader_sim <- VADER(
+  X_train = X_sim,
+  save_path = save_path,
+  n_hidden = lapply(c(n_hidden1, n_hidden2), as.integer),
+  k = as.integer(k),
+  learning_rate = learning_rate,
+  batch_size = as.integer(batch_size),
+  output_activation = NULL,
+  alpha = 1.0,
+  seed = 123L, # makes it reproducible
+  y_train = y_sim,
+  recurrent = TRUE,
+  weights = W_sim
+  # n_thread = 1L,
+  # phi = rep(1/k, k)
+)
+system.time(vader_sim$pre_fit(n_epoch = as.integer(100), verbose = TRUE))
+system.time(vader_sim$fit(n_epoch = as.integer(100), verbose = TRUE))
+
+pdf(file.path(dir_out, "cluster_means_simulated.pdf"), width = 7, height = 7)
+yhat <- y_sim
+n <- ceiling(sqrt(dim(X_sim)[3]))
+par(mfrow = c(n, n))
+YLIM <- list()
+for (i in 1:dim(X_sim)[3]) {
+  dt <- data.table(X_sim[,,i])
+  means <- as.matrix(dt[ , lapply(.SD, mean, na.rm = TRUE), by = yhat])
+  means <- means[order(means[,"yhat"]),]
+  y <- t(means[,-1,drop=FALSE])
+  clusters <- means[,1,drop=FALSE]
+  nc <- ncol(y)
+  # yci <- aggregate(as.matrix(dt), list(yhat), function(x) {
+  #   colSdDiffs(x, na.rm = TRUE) / sqrt(colSums(!is.na(x)) * 2) * 1.96
+  # })
+  yci <- as.matrix(dt[ , lapply(.SD, function(x) {
+    sd(x, na.rm = TRUE) / sqrt(sum(!is.na(x)) * 2) * 1.96
+  }), by = yhat])
+  yci <- yci[order(yci[,"yhat"]),]
+  yci <- t(yci[,-1,drop=FALSE])
+  isna <- is.na(yci) | is.na(y)
+  if (any(isna)) {
+    yci[isna] <- y[isna] <- NA
+  }
+  y <- cbind(y, y - yci, y + yci)
+  x <- as.numeric(rownames(y))
+  YLIM[[i]] <- range(y, na.rm = TRUE)
+  matplot(x, y,
+          type = "l",
+          col = rainbow(nc),
+          lty = rep(c(1, 2, 2), each = nc),
+          lwd = rep(c(3, 1, 1), each = nc),
+          xlab = "Month",
+          ylim = YLIM[[i]],
+          ylab = "z-score relative to baseline",
+          main = sprintf("%s", dimnames(X_sim)[[3]][i])
+  )
+  if (i == dim(X_sim)[3]) {
+    tab <- table(yhat)
+    legend(
+      "bottomleft",
+      legend = c(sprintf("%s (n = %i)", clusters, tab[match(clusters, names(tab))]), "95% CI"),
+      col = c(rainbow(nc), "black"),
+      lty = c(rep(1, nc), 2),
+      lwd = c(rep(3, nc), 1)
+    )
+  }
+}
+par(mfrow = c(1, 1))
+dev.off()
+
+X <- X_sim
+W <- W_sim
+X[W == 0] <- NA
+y <- y_sim
+save(X, W, y, file = file.path(dir_out, "PPMI_artificial_data.RData"))
+X <- do.call(cbind, lapply(1:dim(X)[3], function(i) {
+  x <- X[,,i]
+  colnames(x) <- sprintf("%s_%sm", dimnames(X)[[3]][i], dimnames(X)[[2]])
+  x
+}))
+rownames(X) <- 1:nrow(X)
+dt <- data.table(cbind(cluster = y, X))
+fwrite(dt, file = file.path(dir_out, "PPMI_artificial_data.csv"))
+
